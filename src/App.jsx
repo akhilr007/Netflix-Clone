933ac537df4a57286ce68eae0357970349da1577
Akhil Rajan <rajanakhil07@gmail.com>
2024-01-22 15:13:26 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Body from "./components/Body";
+import Login from "./components/Login";
+import Browse from "./components/Browse";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/browse",
+    element: <Browse />,
+  },
+]);
 
 export default function App() {
-  return <Body />;
+  return (
+    <RouterProvider router={router}>
+      <Body />
+    </RouterProvider>
+  );
 }
