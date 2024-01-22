321b7729ca93932c18b2e32d805035ad2b9819a7
Akhil Rajan <rajanakhil07@gmail.com>
2024-01-22 21:09:26 +0530

    added form validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/validate";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () => {
+    const result = checkValidData(
+      email.current.value,
+      password.current.value,
+      isSignInForm
+    );
+    if (result) setErrorMessage({ ...result });
+  };
 
   return (
     <div>
@@ -14,7 +28,10 @@ const Login = () => {
           alt="background-img"
         />
       </div>
-      <form className="absolute rounded-md w-11/12 sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3 px-6 sm:px-8 md:px-10 py-8 sm:py-10 bg-black mx-auto left-0 right-0 my-24 sm:my-32 bg-opacity-85">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="absolute rounded-md w-11/12 sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3 px-6 sm:px-8 md:px-10 py-8 sm:py-10 bg-black mx-auto left-0 right-0 my-24 sm:my-32 bg-opacity-85"
+      >
         <h1 className="text-white font-bold text-2xl sm:text-3xl my-2 sm:my-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
@@ -29,13 +46,31 @@ const Login = () => {
           className="p-3 my-2 w-full bg-gray-700 text-white rounded-md"
           type="email"
           placeholder="Email"
+          ref={email}
         />
+        {errorMessage !== null && (
+          <p className="text-red-700 font-bold pt-2 pb-2">
+            {errorMessage.emailError}
+          </p>
+        )}
         <input
           className="p-3 my-2 w-full bg-gray-700 text-white rounded-md"
           type="password"
           placeholder="Password"
+          ref={password}
         />
-        <button className="p-4 my-6 bg-red-700 w-full rounded-md text-white">
+        {errorMessage !== null && errorMessage.passwordError && (
+          <div className="text-red-700 font-bold pt-2 pb-2">
+            {errorMessage.passwordError.split("\n").map((line, index) => (
+              <p key={index}>{line}</p>
+            ))}
+          </div>
+        )}
+
+        <button
+          className="p-4 my-6 bg-red-700 w-full rounded-md text-white"
+          onClick={handleButtonClick}
+        >
           {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
         <span className="mt-10 text-gray-500">
---
+++ src/utils/validate.js
@@ -0,0 +1,53 @@
+const passwordRequirements = {
+  minLength: 8,
+  requiresUppercase: true,
+  requiresLowercase: true,
+  requiresNumber: true,
+  requiresSpecialChar: true,
+
+  displayMessage: function () {
+    let message = `Password Requirements:\n`;
+    message += `- Minimum ${this.minLength} characters\n`;
+
+    if (this.requiresUppercase) {
+      message += `- At least 1 uppercase letter (A-Z)\n`;
+    }
+    if (this.requiresLowercase) {
+      message += `- At least 1 lowercase letter (a-z)\n`;
+    }
+    if (this.requiresNumber) {
+      message += `- At least 1 number (0-9)\n`;
+    }
+    if (this.requiresSpecialChar) {
+      message += `- At least 1 special character (!, @, #, $, etc.)\n`;
+    }
+    return message;
+  },
+};
+
+export const checkValidData = (email, password, isSignInForm) => {
+  const isEmailValid = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+  const isPasswordValid =
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
+
+  // Example usage:
+  const passwordMessage = passwordRequirements.displayMessage();
+  let errors = {};
+
+  if (!isEmailValid) {
+    errors = {
+      ...errors,
+      emailError: "Invalid Email. Please enter a valid email.",
+    };
+  }
+  if (!isPasswordValid) {
+    errors = {
+      ...errors,
+      passwordError: isSignInForm
+        ? "Invalid Password. Please enter a valid password."
+        : passwordRequirements.displayMessage(),
+    };
+  }
+
+  return !isEmailValid || !isPasswordValid ? errors : null;
+};
